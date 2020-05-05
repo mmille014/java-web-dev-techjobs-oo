5a78579f720f0536c87248485f84d2cbe2869387
mmille014 <miguel_miller@cable.comcast.com>
2020-05-05 19:19:23 -0400

    Java assignment2 steps1-3completed

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,10 @@
+//package org.launchcode.techjobs_oo.Tests;
+//
+//import org.junit.Before;
+//
+//public class JobTest {
+//    @Before
+//    public void jobObject() {
+//        job = new job("");
+//    }
+//}
