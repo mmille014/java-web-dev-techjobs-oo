f2c7e32b4ff7db4b5953d42cfba5db83dd9381a7
mmille014 <miguel_miller@cable.comcast.com>
2020-05-06 09:30:35 -0400

    Working on Step4 of 6

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,10 +1,13 @@
-//package org.launchcode.techjobs_oo.Tests;
-//
-//import org.junit.Before;
-//
-//public class JobTest {
-//    @Before
-//    public void jobObject() {
-//        job = new job("");
-//    }
-//}
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+//Test the Empty Constructor
+//Use assertEquals, assertTrue, or assertFalse to test that the ID values for the two objects are NOT the same and differ by 1.
+@Test
+public void testJobsForEquality() {
+    assertEquals();
+
+}
+
